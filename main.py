df9fed9db88b4ff9c4f18d98e1bb2c1658b1a729
vuke123 <lukavukelic5@gmail.com>
2024-03-11 16:30:03 +0100

    #00006 - Small errors

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def predict_endpoint():
 if __name__ == '__main__':
     #app.run()
     host = '0.0.0.0'
-    port = 5000
+    port = 80
     httpd = simple_server.make_server(host, port, app)
     httpd.serve_forever()
 
