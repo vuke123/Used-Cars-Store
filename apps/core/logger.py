f120d19f6a6f7943c8f32c38a0a0bf81a4cfc836
vuke123 <lukavukelic5@gmail.com>
2024-03-12 20:43:11 +0100

    #00000 - /

--- apps/core/logger.py
+++ apps/core/logger.py
@@ -1,12 +1,19 @@
 import logging
-
+import os
 class Logger:
 
     def __init__(self, run_id, log_module, log_file_name, mode=logging.DEBUG):
         self.logger = logging.getLogger(str(log_module) + '_' + str(run_id))
         self.logger.setLevel(mode)
         if log_file_name == 'training':
-            file_handler = logging.FileHandler('logs/training_logs/train_log_' + str(run_id) + '.log')
+            #refactor!
+            path = os.path.abspath(__file__)
+            for _ in range(3):
+                path = os.path.dirname(path)
+            project_path = path
+            log_dir = os.path.join(project_path, 'logs', 'training_logs', f"training_log_{run_id}.log")
+            os.makedirs(os.path.dirname(log_dir), exist_ok=True)
+            file_handler = logging.FileHandler(log_dir)
         else:
             file_handler = logging.FileHandler('logs/prediction_logs/predict_log_' + str(run_id) + '.log')
 
